95ea6daf64afe8d6c4fd11681fc43ef56bd369f6
andreyvolobuiev <Andvo1007@gmail.com>
2025-11-03 00:31:56 +0200

    Two Sum Solution

---
+++ Two_Sum/Two_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿using Two_Sum.Entities;
+
+var sol = new Solution();
+int[] solArr = [2, 7, 11, 15];
+
+for (int i = 0; i < sol.TwoSum(solArr, 9).Length; i++)
+{
+    Console.Write($"{sol.TwoSum(solArr, 9)[i]} ");
+}
